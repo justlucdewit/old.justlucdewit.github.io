28bd9c5c82b050feac9b7c716694fa523459747a
luke <luc.dewit@hotmail.nl>
2022-02-27 18:12:04 +0100

    blog page

--- src/Views/Blog.vue
+++ src/Views/Blog.vue
@@ -1,12 +1,24 @@
 <template>
   <Constraint>
-    {{ blogs }}
+    <div class="post" v-for="blog in blogs" :key="blog.fileName">
+      <h1 class="blog-title">{{ blog.title }}</h1>
+
+      <div class="blog-date">
+        {{ blog.date }}
+      </div>
+
+      <div class="blog-content" v-html="md2html(blog.content)">
+      </div>
+    </div>
   </Constraint>
 </template>
 
 <script>
 import axios from "axios";
 import Constraint from '../components/Constraint.vue';
+import MarkdownIt from "markdown-it";
+
+const md = new MarkdownIt();
 
 export default {
   components: {
@@ -24,41 +36,44 @@ export default {
     // Auth token (with only read public permissions)
     const authTok = "anVzdGx1Y2Rld2l0OmdocF9qdDJaOXVoQ055cnpVcWY4ZWhveENxZVNla09xdG8xcE9lZFQ=";
     
-    const getBlogsEndpoint = "https://api.github.com/repos/justlucdewit/justlucdewit.github.io/git/trees/master";
+    const blogEndpoint = "https://raw.githubusercontent.com/justlucdewit/justlucdewit.github.io/master/src/blog/";
 
     // Get list of all blog names
-    let res = (await axios.get(getBlogsEndpoint, {
-      headers: {
-        Authorization: `Basic ${authTok}`
-      }
-    })).data;
+    const blogPostNames = (await axios.get(blogEndpoint + "posts.json")).data;
+    
 
-    const srcFolderEndpoint = res.tree.find(x => x.path === "src").url;
+    const that = this;
+    for (const blogPostName of blogPostNames) {
+      const blogNameParts = blogPostName.split('-');
+      const blogTitle = blogNameParts.pop().split('.md')[0];
+      const blogDate = blogNameParts.join("/");
 
-    res = (await axios.get(srcFolderEndpoint, {
-      headers: {
-        Authorization: `Basic ${authTok}`
-      }
-    })).data;
+      axios.get(blogEndpoint + blogPostName).then(x => {
+        that.blogs.push({
+          fileName: blogPostName,
+          title: blogTitle,
+          content: x.data,
+          date: blogDate
+        });
+      });
+    }
+  },
 
-    this.blogs = res;
-
-    const blogsFolderEndpoint = res.tree.find(x => x.path === "blog").url;
-
-    res = (await axios.get(blogsFolderEndpoint, {
-      headers: {
-        Authorization: `Basic ${authTok}`
-      }
-    })).data;
-
-    this.blogs = res.tree.map(x => x.path);
-
+  methods: {
+    md2html(code) {
+      return md.render(code);
+    }
   }
 }
 </script>
 
 <style Scoped>
-#blog {
-  
+.blog-title {
+  margin: 0px;
+}
+
+.blog-date {
+  color: #777;
+  margin-bottom: 20px;
 }
 </style>
--- src/blog/27-02-2022-First blog post.md
+++ src/blog/27-02-2022-First blog post.md
@@ -1,5 +1,3 @@
-# Welcome to my new blog
-
 Hey and welcome to my new blog!
 
 In the past few days i've been completely remaking my portfolio site. Currently i am completely done with the home and CV page, and started working on the blog page!
